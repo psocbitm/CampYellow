164f962885178bc9e5895d43896aacef92a44bdf
psocbitm <btech10516.19@bitmesra.ac.in>
2022-01-30 09:56:21 +0530

    change crossorigin to crossOrigin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import { useEffect } from "react";
 import Feed from "../components/Feed";
 import Leftbar from "../components/Leftbar";
 import Navbar from "../components/Navbar";
@@ -19,12 +18,12 @@ export default function Home() {
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
 
         {/* fontawesome */}
@@ -32,11 +31,11 @@ export default function Home() {
           rel="stylesheet"
           href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
           integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></link>
         <script
           src="https://kit.fontawesome.com/a30bb40356.js"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
 
         {/* jquery */}
