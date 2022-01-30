fe77b3fd9e6c4be28ca8de7adda256f597adf1e5
psocbitm <btech10516.19@bitmesra.ac.in>
2022-01-30 10:06:06 +0530

    change in likes modal as apostrophe was giving error in deployment

--- components/Post.jsx
+++ components/Post.jsx
@@ -54,7 +54,7 @@ function Post(props) {
           <div className="modal-dialog">
             <div className="modal-content">
               <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">Likes on {props.post.username}'s post</h5>
+                <h5 className="modal-title" id="exampleModalLabel">Likes on {props.post.username} post</h5>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div className="modal-body">
@@ -67,7 +67,6 @@ function Post(props) {
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="btn btn-primary">Save changes</button>
               </div>
             </div>
           </div>
